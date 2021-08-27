bfa98ffee65ec66b696a9d84cae82f9ff8bb536b
Tristan B. Kildaire <deavmi@redxen.eu>
2021-08-27 15:53:19 +0200

    Added stub for Engine class

---
+++ source/eventy/engine.d
@@ -0,0 +1,17 @@
+module eventy.engine;
+
+
+/**
+* Engine
+*
+* An instance of this represents an engine that
+* can, at any time, handle the delivery of new
+* events, trigger the correct signal handlers
+* for the respective events, remove signal
+* handlers, add signal handlers, among many
+* other things
+*/
+public final class Engine
+{
+
+}
