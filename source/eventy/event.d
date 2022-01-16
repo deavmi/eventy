c2bc5bc902a755c0a7cd9504c5f94e363c818df0
Tristan B. Kildaire <deavmi@redxen.eu>
2022-01-16 14:21:09 +0200

    Added optional payload support

--- source/eventy/event.d
+++ source/eventy/event.d
@@ -5,16 +5,21 @@ module eventy.event;
 *
 * An Event represents a trigger for a given signal(s)
 * handlers which associate with the given typeID
+*
+* It can optionally take a payload with it as well
 */
 public class Event
 {
     /**
-    *
+    * Creates a new Event, optionally taking with is a
+    * payload
     */
-    this(ulong typeID)
+    this(ulong typeID, ubyte[] payload = null)
     {
         this.id = typeID;
+        this.payload = payload;
     }
 
     ulong id;
+    ubyte[] payload;
 }
