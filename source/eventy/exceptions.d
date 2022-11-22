9aea5aa12b4be723b28bbaad2a0e349d5e45c1df
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2022-11-22 17:41:09 +0200

    Removed all code

--- source/eventy/exceptions.d
+++
@@ -1,9 +0,0 @@
-module eventy.exceptions;
-
-public final class EventyException : Exception
-{
-    this(string message)
-    {
-        super(message);
-    }
-}
--- source/eventy/package.d
+++
@@ -1,7 +0,0 @@
-module eventy;
-
-public import eventy.event;
-public import eventy.exceptions;
-public import eventy.engine;
-public import eventy.queues;
-public import eventy.signal;
