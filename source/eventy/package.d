27db1734f12022edbaa172d3ab73e82fae119cc6
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-01-21 18:33:46 +0200

    Removed old code for a new slate

--- source/eventy/package.d
+++ source/eventy/package.d
@@ -1,8 +1,2 @@
 module eventy;
 
-public import eventy.event;
-public import eventy.exceptions;
-public import eventy.engine;
-public import eventy.types;
-public import eventy.signal;
-public import eventy.config;
